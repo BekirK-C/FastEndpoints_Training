34d05d4b781ee79091d9e88443bb1f137d1e31be
Bekir Berat Kamacı <b.b.kamac@gmail.com>
2023-10-28 20:34:08 +0300

    Basic Example of REPR

---
+++ FE.API/Endpoints/MyEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+using FE.API.Requests;
+using FE.API.Responses;
+
+namespace FE.API.Endpoints;
+
+public class MyEndpoint : Endpoint<MyRequest, MyResponse>
+{
+    public override void Configure()
+    {
+        Post("/api/user/create");
+        AllowAnonymous();
+    }
+    public override async Task HandleAsync(MyRequest req, CancellationToken ct)
+    {
+        await SendAsync(new MyResponse
+        {
+            FullName = $"{req.FirstName} {req.LastName}",
+            IsOver18 = req.Age >= 18
+        });
+    }
+}
---
+++ FE.API/Requests/MyRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace FE.API.Requests;
+public class MyRequest
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
+
